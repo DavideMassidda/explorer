f9e3e53be44596f879167cff47c1bee03fda8ca8
Davide Massidda <davide.massidda@gmail.com>
2014-11-30 21:53:09 +0100

    commit 30.11.14

--- R/cor.matrix.R
+++ R/cor.matrix.R
@@ -2,10 +2,10 @@ cor.matrix <-
 function(x,y=NULL,z=NULL,na.action=c("listwise.deletion","pairwise.deletion","mean.replace"),method=c("pearson","kendall","spearman"))
 {
     # Verifica di na.action
-    na.action <- tolower(na.action)
+    na.action <- tolower(na.action[1])
     na.action <- match.arg(na.action)
     # Verifica di method
-    method <- tolower(method)
+    method <- tolower(method[1])
     method <- match.arg(method)
     if(is.vector(x)) {
         x.name <- deparse(substitute(x))
